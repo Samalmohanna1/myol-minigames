4f160167a83ff47adf6eac0d71198b9659ff8490
Sam Almohanna <webdevsumayah@gmail.com>
2025-02-17 15:04:56 -0500

    initial commit & setup

---
+++ src/data/baseData.js
@@ -0,0 +1,11 @@
+export default {
+	title: `MYOL MiniGames | low commitment games`,
+	description: `MYOL MiniGames description.`,
+	url: `https://www.myol-creative.com`, // No trailing slash!
+	// JSON LD
+	name: `MYOL MiniGames | low commitment games`,
+	//OG Image
+	ogImage: '/og-image.png',
+	// Github URL
+	githubURL: `https://github.com/samalmohanna1`,
+}
